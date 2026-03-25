03e5aef018e0d343ffe3c3049995336c2a3a4b1c
aeongiing <ywjk0515@gmail.com>
2026-03-26 03:05:13 +0900

    fix: 코드 포맷터 재설정 후 코드 정렬

--- practice/chapter02/practice01/src/App.tsx
+++ practice/chapter02/practice01/src/App.tsx
@@ -3,24 +3,30 @@ import "./App.css";
 import List from "./components/List";
 
 function App() {
-	const nickname = "매튜";
-	const sweetPotato = "고구마";
-	const array = ["REACT", "NEXT", "VUE", "SVELTE", "ANGULAR", "REACT-NATIVE"];
-	return (
-		<>
-			<strong className="school">상명대학교</strong>
-			<p style={{ color: "purple", fontWeight: "bold", fontSize: "3rem" }}>
-				{nickname}/김용민
-			</p>
-			<h1>{`${nickname}는 ${sweetPotato} 아이스크림을 좋아합니다.`}</h1>
-			<ul>
-				{array.map((yaho, idx) => (
-					// 2) <li key={idx}>{yaho}</li> → <List />로 교체해요.
-					<List key={idx} tech={yaho} />
-				))}
-			</ul>
-		</>
-	);
+  const nickname = "매튜";
+  const sweetPotato = "고구마";
+  const array = ["REACT", "NEXT", "VUE", "SVELTE", "ANGULAR", "REACT-NATIVE"];
+  return (
+    <>
+      <strong className="school">상명대학교</strong>
+      <p
+        style={{
+          color: "purple",
+          fontWeight: "bold",
+          fontSize: "3rem",
+        }}
+      >
+        {nickname}/김용민
+      </p>
+      <h1>{`${nickname}는 ${sweetPotato} 아이스크림을 좋아합니다.`}</h1>
+      <ul>
+        {array.map((yaho, idx) => (
+          // 2) <li key={idx}>{yaho}</li> → <List />로 교체해요.
+          <List key={idx} tech={yaho} />
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default App;
